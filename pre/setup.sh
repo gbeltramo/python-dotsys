c70ff5e21aa49252f0bb936331e0c452394a5fd9
Gabriele Beltramo <hello@gbeltramo.com>
2024-04-04 18:20:47 +0200

    Update

--- pre/setup.sh
+++ pre/setup.sh
@@ -3,13 +3,13 @@
 # 1
 sudo apt update
 sudo apt full-upgrade
-export P_VERSION=3.11 
+export P_VERSION=3.11
 sudo apt install --yes python$P_VERSION python$P_VERSION-dev python$P_VERSION-venv
 
 # 2
 /usr/bin/python$P_VERSION -m venv $HOME/.local/python-venv
 $HOME/.local/python-venv/bin/python$P_VERSION -m pip install -U setuptools wheel pip
-$HOME/.local/python-venv/bin/python$P_VERSION -m pip install termcolor black numpy build pytest
+$HOME/.local/python-venv/bin/python$P_VERSION -m pip install termcolor black numpy build pytest pydub
 
 # 3
 mkdir -p $HOME/.local/python-venv/pybin/ && cd $HOME/.local/python-venv/pybin/
