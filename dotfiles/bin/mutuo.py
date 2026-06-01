8bbaced2c25da7d086319d0e7f51f2a04ad00994
Gabriele Beltramo <hello@gbeltramo.com>
2026-06-01 10:34:02 +0200

    Remove deps from mutuo.py

--- dotfiles/bin/mutuo.py
+++ dotfiles/bin/mutuo.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env p
 import argparse
 
-from easydict import EasyDict
-from termcolor import colored
-
-
 def parse_arguments() -> dict:
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -23,14 +19,13 @@ def parse_arguments() -> dict:
     parser.add_argument("--taken", type=int, required=True, help="Money you take")
     arguments = parser.parse_args()
 
-    args = EasyDict(
-        {
-            "TAN": arguments.TAN,
-            "TAEG": arguments.TAEG,
-            "num_years": arguments.num_years,
-            "taken": arguments.taken,
+    args = {
+           "TAN": arguments.TAN,
+           "TAEG": arguments.TAEG,
+           "num_years": arguments.num_years,
+           "taken": arguments.taken,
         }
-    )
+    
     return args
 
 
@@ -40,21 +35,21 @@ def main(args: dict) -> None:
     results = {"TAN": dict(), "TAEG": dict()}
     for rate in results.keys():
         r = args[rate] / 12.0 / 100.0
-        n = args.num_years * 12
-        monthly_fee = round(args.taken * (r * (1 + r) ** n) / ((1 + r) ** n - 1), 2)
+        n = args["num_years"] * 12
+        monthly_fee = round(args["taken"] * (r * (1 + r) ** n) / ((1 + r) ** n - 1), 2)
         total = round(monthly_fee * n, 2)
-        results[rate] = {"total": total, "cost": round(total - args.taken, 2)}
+        results[rate] = {"total": total, "cost": round(total - args["taken"], 2)}
 
         # Results to STDOUT
-        print(colored("-" * 30, "light_blue"))
-        print(colored(f"Calculation with {rate} of {args[rate]}", "light_blue"))
-        print(colored("\t- Money you take:", "cyan"), f"{args.taken}")
-        print(colored("\t- Monthly fee:", "cyan"), round(monthly_fee, 2))
-        print(colored("\t- Total you give back:", "cyan"), f"{results[rate]['total']}")
-        print(colored("\t- Cost:", "cyan"), f"{results[rate]['cost']}")
+        print("-" * 30)
+        print(f"Calculation with {rate} of {args[rate]}")
+        print(f"\t- Money you take: {args['taken']}")
+        print("\t- Monthly fee:", round(monthly_fee, 2))
+        print("\t- Total you give back:", f"{results[rate]['total']}")
+        print("\t- Cost:", f"{results[rate]['cost']}")
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    print(colored("Arguments:", "light_blue"), args)
+    print("Arguments:", args)
     main(args=args)
