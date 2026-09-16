01be27757b5422ad33c4304b0cc12f1dc1431084
Gabriele Beltramo <hello@gbeltramo.com>
2026-09-16 15:52:01 +0200

    Update shell/config.fish zed/settings.json and create tools/kanban.html

--- dotfiles/shell/config.fish
+++ dotfiles/shell/config.fish
@@ -1,3 +1,8 @@
+if test -f /opt/homebrew/bin/brew
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+fish_add_path /opt/homebrew/opt/git/bin
+
 fish_add_path $HOME/Lang/flutter/bin
 fish_add_path $HOME/Lang/zig/bin
 fish_add_path /usr/local/go/bin
@@ -32,16 +37,35 @@ function e
     emacs -Q -nw --eval "(progn (load-theme 'tango-dark t) (defalias 'yes-or-no-p 'y-or-n-p) (global-display-line-numbers-mode t) (ido-mode t) (setq echo-keystrokes 0.1) (setq org-hide-emphasis-markers t) (setq org-src-fontify-natively t) (setq org-fontify-quote-and-verse-blocks t) (setq org-src-tab-acts-natively t) (setq org-edit-src-content-indentation 2) (setq org-hide-block-startup nil) (setq org-src-preserve-indentation nil) (setq org-startup-folded 'content) (setq org-cycle-separator-lines 2) (setq org-startup-with-inline-images t))" $argv
 end
 
+function _dog_impl
+    set -l style $argv[1]
+    set -e argv[1]
+
+    set -l cmd
+    if type -q bat
+        set cmd bat
+    else if type -q batcat
+        set cmd batcat
+    else
+        echo "Neither bat nor batcat found" >&2
+        return 1
+    end
+
+    $cmd --paging="always" --pager="less -R" --style="$style" --wrap="never" --theme="Monokai Extended Bright" $argv
+end
+
 function dog
-    bat --paging="always" --pager="less -R" --style="header" --wrap="never" --theme="Monokai Extended Bright" $argv
+    _dog_impl header $argv
 end
 
 function dog-full
-    bat --paging="always" --pager="less -R" --style="full" --wrap="never" --theme="Monokai Extended Bright" $argv
+    _dog_impl full $argv
 end
 
 if test -f ~/.venv/bin/activate.fish
     source ~/.venv/bin/activate.fish
 end
 
-source "$HOME/.cargo/env.fish"
+if test -f "$HOME/.cargo/env.fish"
+    source "$HOME/.cargo/env.fish"
+end
